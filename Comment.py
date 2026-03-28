4fe1c98b3a1af925311c97ce5154a1f18f076866
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-03-28 09:48:31 +0530

    main

---
+++ Comment.py
@@ -0,0 +1,29 @@
+#Comments in Python:
+
+#Explaination:
+
+#Comments are used to explain the code.
+#Comments are not executed by the Python interpreter.
+#Comments are used to make the code more readable.
+#Comments are used to document the code.
+#Comments are used to test the code.
+
+#Types of Comments:
+
+#1. Single-line comments
+#2. Multi-line comments
+#3. Docstrings
+
+#Example of single-line comment:
+
+#This is a single-line comment
+
+#Example of multi-line comment:
+
+#This is a multi-line comment
+#This is a multi-line comment
+#This is a multi-line comment
+
+#Example of docstring:
+
+"""This is a docstring"""
